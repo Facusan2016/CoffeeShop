67a4987251ae9d83d6eedb34165ccf15ccc1b01e
Facusan2016 <facusan2016@gmail.com>
2023-08-11 00:35:56 -0300

    fix deprecated RESTAPI

--- src/mainPage/helpers/getCoffeeData.js
+++ src/mainPage/helpers/getCoffeeData.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
-const url = 'https://api.sampleapis.com/coffee/hot'
+const url = 'http://localhost:3500/get-data'
 
 export const getCoffeeData = async() =>{
     
     try {
 
         const {data} = await axios.get(url); //Axios request to the url to get coffeData
-        const prices = [1, 1.25, 2, 1.50, 2.50, 2.75, 1.75, 2.25, 3, 3.50, 1, 1.25, 2, 1.50, 2.50, 2.75, 1.75, 2.25, 3, 3.50]
-        //I'm not the owner of this api rest, so i added my local prices to each one.
+        const prices = [1, 1.25, 2, 1.50, 2.50, 2.75, 1.75, 2.25, 3, 3.50, 1, 1.25, 2, 1.50, 2.50, 2.75, 1.75, 2.25, 3, 3.50]//I'm not the owner of this api rest, so i added my local prices to each one.
+        const {resp} = data;
         
     
-        const coffees = data.map(e => (
+        const coffees = resp.map(e => (
     
             {
-                id: e.id,
+                id: e._id,
                 id_name : e.title.split(/[ :-]/).join(''), 
                 name : e.title,
                 price : (e.id <= prices.length) ? prices[e.id - 1] : 2,
